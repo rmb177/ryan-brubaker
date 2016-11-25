56b2ccd3885d0f03f976da5dbbede229f199b437
Ryan Brubaker <ryan.brubaker@gmail.com>
2016-11-25 10:28:58 -0600

    Added page for morse code simulator

--- gae/src/pages/doodles/morse_code/morse_code.py
+++ gae/src/pages/doodles/morse_code/morse_code.py
@@ -3,8 +3,11 @@ import os
 import jinja2
 import webapp2
 
+import src.pages.base as base
+
+
 JINJA_ENV = jinja2.Environment(
- loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+ loader=jinja2.FileSystemLoader(base.BASE_PATH),
  extensions=['jinja2.ext.autoescape'],
  autoescape=True)
 
@@ -13,9 +16,9 @@ class MorseCode(webapp2.RequestHandler):
    Displays the Morse Code demo
    """
    def get(self):
-      template = JINJA_ENV.get_template('morse_code.html')
+      template = JINJA_ENV.get_template('doodles/morse_code/morse_code.html')
       self.response.write(template.render())
    #
 #
 
-app = webapp2.WSGIApplication([('/fun/morse-code/?', MorseCode)])
+app = webapp2.WSGIApplication([('/doodles/morse_code/?', MorseCode)])
